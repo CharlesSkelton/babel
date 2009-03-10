4c7e56618c0f82faae1acc8d79d42504e1e4906c
cskelton <cskelton@b387b25f-291c-0410-8dc7-e51caacd5b47>
2009-03-10 12:51:26 +0000

    released to the kx community

---
+++ readme.txt
@@ -0,0 +1,3 @@
+To run this application from the command line without Ant, try:
+
+java -cp "libs/images.jar:libs/jcommon-0.9.6.jar:libs/jfreechart-0.9.21.jar:dist/jshow.jar" Show
---
+++ src/de/skelton/util/SmartArray.java
@@ -0,0 +1,96 @@
+package de.skelton.util;
+
+import java.lang.reflect.Array;
+import java.sql.Date;
+import java.sql.Time;
+import java.sql.Timestamp;
+
+public class SmartArray
+{
+    int writePosition=0;
+    private Object array;
+    private int growthSize;
+
+    public SmartArray(Class primitiveClass)
+    {
+        this(1024,primitiveClass);
+    }
+
+    public SmartArray(int initialSize,Class primitiveClass)
+    {
+        this(initialSize,(int)(initialSize/4),primitiveClass);
+    }
+
+    public SmartArray(int initialSize,int growthSize,Class primitiveClass)
+    {
+        this.growthSize=growthSize;
+        array=Array.newInstance(primitiveClass,initialSize);
+    }
+
+    private void checkCapacity()
+    {
+        if(writePosition>=Array.getLength(array))
+        {
+            Object tmpArray=Array.newInstance(array.getClass().getComponentType(),Array.getLength(array)+growthSize);
+            System.arraycopy(array,0,tmpArray,0,Array.getLength(array));
+            array=tmpArray;
+        }
+    }
+
+    public void append(Time t)
+    {
+        Array.set(array,writePosition++,t);
+    }
+
+    public void append(Timestamp t)
+    {
+        Array.set(array,writePosition++,t);
+    }
+
+    public void append(Date d)
+    {
+        Array.set(array,writePosition++,d);
+    }
+
+    public void append(short s)
+    {
+        Array.setShort(array,writePosition++,s);
+    }
+
+    public void append(long l)
+    {
+        Array.setLong(array,writePosition++,l);
+    }
+
+    public void append(boolean b)
+    {
+        Array.setBoolean(array,writePosition++,b);
+    }
+
+    public void append(float f)
+    {
+        Array.setFloat(array,writePosition++,f);
+    }
+
+    public void append(double d)
+    {
+        Array.setDouble(array,writePosition++,d);
+    }
+
+    public void append(int i)
+    {
+        Array.setInt(array,writePosition++,i);
+    }
+
+    public void append(String s)
+    {
+        Array.set(array,writePosition++,s);
+    }
+
+    public Object toArray()
+    {
+        Object trimmedArray=Array.newInstance(array.getClass().getComponentType(),writePosition);
+        System.arraycopy(array,0,trimmedArray,0,writePosition);
+        return trimmedArray;
+    }
+}
