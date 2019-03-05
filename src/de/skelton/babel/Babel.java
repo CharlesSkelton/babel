eee43f4290f1c52f6f50cc583cbc7f13536fcfa9
CharlesSkelton <CharlesSkelton@users.noreply.github.com>
2019-03-05 10:51:50 +0100

    Update Babel.java

--- src/de/skelton/babel/Babel.java
+++ src/de/skelton/babel/Babel.java
@@ -1,6 +1,17 @@
-/* Babel for kdb+ by Charles Skelton
-   is licensed under a Creative Commons Attribution-Noncommercial-Share Alike 3.0 Germany License
-   http://creativecommons.org/licenses/by-nc-sa/3.0
+/* Babel for kdb+
+   Copyright 2019 Charles Skelton
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
 */
 package de.skelton.babel;
 
